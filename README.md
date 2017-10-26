791425e06647851f17809dbc22791011d03df310
Dave <dkelly1818@yahoo.com>
2017-10-26 19:05:39 -0400

    Uploading content

---
+++ README.md
@@ -0,0 +1,11 @@
+# comic-sans-everything
+
+Chrome Extention that turns all website font to Comic Sans MS.
+
+## Non Windows or MacOS Systems
+
+You probably need to [install Comic Sans](http://www.howtogeek.com/howto/15495/add-microsoft-core-fonts-to-ubuntu/).
+
+## License
+
+MIT
---
+++ background.js
@@ -0,0 +1,31 @@
+'use strict'
+
+let fontOverwrite = '* {font-family: Comic Sans MS !important}'
+let transformOverwrite = '* {text-transform:uppercase}'
+
+// if we dont have the styleDom, create it
+let styleDom = document.getElementById('comic-sans-everything-style')
+if (!styleDom) {
+  styleDom = document.querySelector('head').appendChild(document.createElement('style'))
+  styleDom.id = 'comic-sans-everything-style'
+  styleDom.rel = 'stylesheet'
+  styleDom.type = 'text/css'
+}
+const updateStyle = () => {
+  window.chrome.storage.sync.get(['status', 'uppercase'], (items) => {
+    if (!window.chrome.runtime.error) {
+      if (items.status && items.uppercase) {
+        styleDom.innerText = fontOverwrite + transformOverwrite
+      } else if (items.status) {
+        styleDom.innerText = fontOverwrite
+      } else {
+        styleDom.innerText = ''
+      }
+    }
+  })
+}
+
+// run once on file load
+updateStyle()
+
+window.chrome.runtime.onMessage.addListener(updateStyle)
